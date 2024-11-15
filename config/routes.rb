ce17ff31d025b7481c28ff2d4387c2bfbff81b52
b.s <brandou35000@gmail.com>
2024-11-15 16:55:27 +0100

    Add dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "lists#index"
   resources :lists do
-    resources :movie, only: [ :index ]
+    resources :bookmarks, only: [ :new, :create ]
   end
 end
